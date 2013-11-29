e2c30228513debbf1cf4939620158a62ac0c48fe
TatyanaM <t_mudryak@mail.ru>
2013-11-29 15:24:40 +0700

    FlickrFetcher

---
+++ СoreDataSPoT/FlickrFetcher/FlickrAPIKey.h
@@ -0,0 +1,12 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Winter 2013.
+//  Copyright 2013 Stanford University
+//  All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"8d7edd2250b28c5bed3b4e77ccfca48f"
